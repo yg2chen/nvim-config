59281f66496b883ab7bea76b5f5f4e69fa708a4c
yg2chen <yco5.chen@gmail.com>
2025-01-09 18:00:56 +0800

    add terminal configurations

---
+++ lua/custom/plugins/terminal.lua
@@ -0,0 +1,17 @@
+-- :tab term
+local set = vim.opt_local
+
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = vim.api.nvim_create_augroup('Open_Terminal', {}),
+  callback = function()
+    set.nu = false
+    set.relativenumber = false
+    set.scrolloff = 0
+    vim.bo.filetype = 'terminal'
+  end,
+})
+
+-- escape in terminal mode
+vim.keymap.set('t', '<esc><esc>', '<c-\\><c-n>')
+
+return {}
