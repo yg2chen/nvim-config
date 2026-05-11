24e44fc3f8b54de39b611cb454aa439527b6a4b0
yg2chen <yco5.chen@gmail.com>
2026-05-11 18:07:28 +0800

    tweaks to colorscheme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,5 +1,11 @@
+local colors = require 'ayu.colors'
+colors.generate(true)
+
 require('ayu').setup {
     mirage = true,
+    overrides = {
+        CursorLineNr = { fg = '#FFCC66' },
+    },
 }
 
 -- require('gruvbox').setup {
